fbc0156276d8039593a3d27f30371ece1c718392
nautics889 <cyberukr@gmail.com>
2018-02-10 23:38:29 +0300

    getting backup

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ token = '503545354:AAFUYzbkH2e-Ek7iIVGPj2G7MooJXO0I8to'
 URL_VK = 'https://api.vk.com/method/wall.get?domain=publ59954653&count=10&filter=owner&access_token=08f787df9d0ee70fcf7e973732ce51daaffd2e73145484affef61111ca7adb73238a8585dfc0d8c1fccd5'
 FILENAME_VK = 'last_known_id.txt'
 BASE_POST_URL = 'https://vk.com/wall-59954653_'
-CHANNEL_NAME = '@tspp'
+CHANNEL_NAME = '@tspp654567'
 SINGLE_RUN = False
